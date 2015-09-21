91fb173a5a55049d15e91016cc98e9d28ca24224
MiYABiS <moka@miyabis.jp>
2015-09-22 00:06:02 +0900

    新規追加

---
+++ ProjectTemplateWindowsForm/tools/install.ps1
@@ -0,0 +1,18 @@
+﻿param($installPath, $toolsPath, $package, $project)
+
+
+if ($project.CodeModel.Language -eq "{B5E9BD34-6D3E-4B5D-925E-8A43B79820B4}") 
+{
+	$item = $project.ProjectItems | where-object {$_.Name -eq "ApplicationEvents.vb"}
+	$item.Delete()
+}
+if ($project.CodeModel.Language -eq "{B5E9BD33-6D3E-4B5D-925E-8A43B79820B4}") 
+{
+	$item = $project.ProjectItems | where-object {$_.Name -eq "Program.cs.txt"}
+	$item.Delete()
+}
+
+Add-Type -AssemblyName 'Microsoft.Build, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a'
+$msbuild = [Microsoft.Build.Evaluation.ProjectCollection]::GlobalProjectCollection.GetLoadedProjects($project.FullName) | Select-Object -First 1
+
+$project.Save()
---
+++ README.md
@@ -0,0 +1,18 @@
+# ProjectTemplateWindowsForm
+
+Moca.NET Project Template Windows Form
+
+How to get
+==========
+
+URL:https://www.nuget.org/packages/Moca.NETWindowsFormProject/
+```
+PM> Install-Package Moca.NETWindowsFormProject
+```
+
+License
+=======
+
+Microsoft Public License (MS-PL)
+
+http://opensource.org/licenses/MS-PL
